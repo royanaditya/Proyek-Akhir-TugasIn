e9ce045cd5d10d0ff5c2e8063c1db6df0fcf4b46
veriaw <ferianggoro.aw@gmail.com>
2025-05-25 19:43:43 +0700

    fix the port

--- backend/Index.js
+++ backend/Index.js
@@ -9,7 +9,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 // Konfigurasi CORS agar mengizinkan domain frontend terdeploy
 const corsOptions = {
